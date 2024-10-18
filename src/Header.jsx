3652e6925a935e35fdccebcdbc566b287c2c08df
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2024-10-18 09:40:22 -0300

    add

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header>
+            <div className="logo">
+                <a href="/">
+                    <img src="img/logo.svg" alt="Logo do Digital Bank" />
+                </a>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
